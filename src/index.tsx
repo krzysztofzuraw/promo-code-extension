aa6a0df4ded161710432c57e8a3a75b1e50dabf3
Krzysztof Żuraw <services@kzuraw.com>
2020-07-02 18:18:33 +0200

    feat: display current web url

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,11 @@ import React, { FunctionComponent } from 'react';
 import { render } from 'react-dom';
 
 const Root: FunctionComponent = () => {
-  return <div>Super cool working!</div>;
+  const [url, setUrl] = React.useState('');
+  browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
+    setUrl(tabs[0].url ?? '');
+  });
+  return <div>Super cool working! {url}</div>;
 };
 
 render(<Root />, document.getElementById('root'));
