090a2b0152df7a69aa8d30a0a36e82f2db7fc7d9
Krzysztof Żuraw <services@kzuraw.com>
2020-07-20 18:40:40 +0200

    update

--- src/add-new.tsx
+++ src/add-new.tsx
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { FunctionComponent, useState } from 'react';
 
-export const AddNew: React.FunctionComponent = () => {
-  return <div>Werks</div>;
+export const AddNew: FunctionComponent = () => {
+  const [url, setUrl] = useState('');
+  const [code, setCode] = useState('');
+  const [date, setDate] = useState('');
+
+  return (
+    <div>
+      <form>
+        <div className="field">
+          <label htmlFor="url">URL address</label>
+          <input
+            name="url"
+            id="url"
+            value={url}
+            type="url"
+            onChange={(e) => setUrl(e.target.value)}
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="code">Promo code</label>
+          <input
+            name="code"
+            id="code"
+            type="text"
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="date">Valid until</label>
+          <input
+            name="date"
+            id="date"
+            value={date}
+            type="date"
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
