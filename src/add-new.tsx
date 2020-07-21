c81ad4828158aac49330b5e713a34eb743c1e7f6
Krzysztof Żuraw <services@kzuraw.com>
2020-07-21 19:20:15 +0200

    add clear icon

--- src/add-new.tsx
+++ src/add-new.tsx
@@ -10,6 +10,7 @@ export const AddNew: FunctionComponent = () => {
       <form>
         <div className="field">
           <label htmlFor="url">URL address</label>
+          {url && <ClearIcon onClick={() => setUrl('')} />}
           <input
             name="url"
             id="url"
@@ -20,6 +21,7 @@ export const AddNew: FunctionComponent = () => {
         </div>
         <div className="field">
           <label htmlFor="code">Promo code</label>
+          {code && <ClearIcon onClick={() => setCode('')} />}
           <input
             name="code"
             id="code"
@@ -42,3 +44,19 @@ export const AddNew: FunctionComponent = () => {
     </div>
   );
 };
+
+const ClearIcon: FunctionComponent<{ onClick?: () => void }> = ({ onClick }) => (
+  <svg
+    onClick={onClick}
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    className="clear"
+  >
+    <path
+      fill="context-fill"
+      d="M6.586 8l-2.293 2.293a1 1 0 0 0 1.414 1.414L8 9.414l2.293 2.293a1 1 0 0 0 1.414-1.414L9.414 8l2.293-2.293a1 1 0 1 0-1.414-1.414L8 6.586 5.707 4.293a1 1 0 0 0-1.414 1.414L6.586 8zM8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0z"
+    ></path>
+  </svg>
+);
