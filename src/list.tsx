bae0587d043cd2ad544a7d88965a78bc8f5dd218
Krzysztof Żuraw <services@kzuraw.com>
2020-08-03 18:38:16 +0200

    feat: migrate to chrome

--- src/list.tsx
+++ src/list.tsx
@@ -1,15 +1,37 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import { usePopper } from 'react-popper';
 
 export const List: FunctionComponent = () => {
+  const [copied, setCopied] = React.useState(false);
+  const [referenceElement, setReferenceElement] = useState<any>(null);
+  const [popperElement, setPopperElement] = useState<any>(null);
+  const [arrowElement, setArrowElement] = useState<any>(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    modifiers: [{ name: 'arrow', options: { element: arrowElement } }],
+  });
   return (
     <>
       <ul>
         <li>
-          <div className="code" title="click to copy">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro neque debitis expedita
-            deserunt tempore, repudiandae quae, veritatis deleniti sapiente asperiores ducimus
-            perferendis hic blanditiis corporis temporibus dolorem, accusamus earum similique.
-          </div>
+          {copied && (
+            <div ref={setPopperElement} style={styles.popper} {...attributes.popper}>
+              Popper element
+              <div ref={setArrowElement} style={styles.arrow} />
+            </div>
+          )}
+          <CopyToClipboard text="lorem code">
+            <div
+              className="code"
+              title="Click to copy"
+              ref={setReferenceElement}
+              onClick={() => setCopied(true)}
+            >
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro neque debitis expedita
+              deserunt tempore, repudiandae quae, veritatis deleniti sapiente asperiores ducimus
+              perferendis hic blanditiis corporis temporibus dolorem, accusamus earum similique.
+            </div>
+          </CopyToClipboard>
           <div className="valid-date align-right">Valid until 12/03/2020</div>
           <div className="url">https://www.onet.pl</div>
           <button className="button-default align-right">Edit</button>
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -18,6 +19,9 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: './src/manifest.json' }],
+    }),
   ],
   output: { filename: '[name].js', path: path.resolve(__dirname, 'dist') },
 };
